b54ad1537fcf1af0442b1d2369ecbfd46d7be707
msriv <msrivastava574@gmail.com>
2020-11-28 18:31:00 +0530

    UI improvement activity_about_us.xml and activity_feedback.xml

--- app/src/main/java/com/recipe/project/AboutUs.java
+++ app/src/main/java/com/recipe/project/AboutUs.java
@@ -1,11 +1,15 @@
 package com.recipe.project;
 
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 public class AboutUs extends AppCompatActivity {
 
@@ -14,6 +18,65 @@ public class AboutUs extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        CardView cardViewMihir;
+        CardView cardViewHatim;
+        CardView cardViewAvi;
+
+        cardViewAvi = findViewById(R.id.imageCardAvi);
+        cardViewHatim = findViewById(R.id.imageCardHatim);
+        cardViewMihir = findViewById(R.id.imageCardMihir);
+        String baseUrl = "https://www.linkedin.com/in/";
+
+        cardViewAvi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String finalUrl = baseUrl.concat("avichoukhany");
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(finalUrl));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.setPackage("com.android.chrome");
+                try {
+                    view.getContext().startActivity(intent);
+                } catch (ActivityNotFoundException ex) {
+                    // Chrome browser presumably not installed so allow user to choose instead
+                    intent.setPackage(null);
+                    view.getContext().startActivity(intent);
+                }
+            }
+        });
+        cardViewHatim.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String finalUrl = baseUrl.concat("hatim-nomani");
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(finalUrl));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.setPackage("com.android.chrome");
+                try {
+                    view.getContext().startActivity(intent);
+                } catch (ActivityNotFoundException ex) {
+                    // Chrome browser presumably not installed so allow user to choose instead
+                    intent.setPackage(null);
+                    view.getContext().startActivity(intent);
+                }
+            }
+        });
+        cardViewMihir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String finalUrl = baseUrl.concat("mihir-srivastava");
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(finalUrl));
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                intent.setPackage("com.android.chrome");
+                try {
+                    view.getContext().startActivity(intent);
+                } catch (ActivityNotFoundException ex) {
+                    // Chrome browser presumably not installed so allow user to choose instead
+                    intent.setPackage(null);
+                    view.getContext().startActivity(intent);
+                }
+            }
+        });
+
     }
 
     @Override
