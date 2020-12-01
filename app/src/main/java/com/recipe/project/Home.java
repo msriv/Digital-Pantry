f93fd8ae641012e93891243f44624a9ac96a9046
Hatim Nomani <hatim@digitaltakeoff.in>
2020-12-01 15:40:10 +0530

    :sparkles: tf class and camera function

--- app/src/main/java/com/recipe/project/Home.java
+++ app/src/main/java/com/recipe/project/Home.java
@@ -1,17 +1,28 @@
 package com.recipe.project;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.DividerItemDecoration;
@@ -21,9 +32,11 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.gson.JsonElement;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import ai.api.android.AIConfiguration;
 import ai.api.android.AIService;
@@ -41,6 +54,137 @@ public class Home extends AppCompatActivity {
 
     private final List<String> iList = new ArrayList<>();
     InputAdapter mAdapter;
+
+    private static final int CAMERA_PERMISSION_REQUEST_CODE = 1000;
+    private static final int CAMERA_REQEUST_CODE = 10001;
+    /**
+     * UI Elements
+     */
+    private ImageView imageView;
+    private ListView listView;
+    private ImageClassifier imageClassifier;
+
+    /**
+     * Method to initalize UI Elements. this method adds the on click
+     */
+    private void initializeUIElements() {
+        imageView = findViewById(R.id.modelimage);
+        listView = findViewById(R.id.list);
+        Button takepicture = findViewById(R.id.camera);
+        /*
+         * Creating an instance of our tensor image classifier
+         */
+        try {
+            imageClassifier = new ImageClassifier(this);
+        } catch (IOException e) {
+            Log.e("Image Classifier Error", "ERROR: " + e);
+        }
+        // adding on click listener to button
+        takepicture.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // checking whether camera permissions are available.
+                // if permission is avaialble then we open camera intent to get picture
+                // otherwise reqeusts for permissions
+                if (hasPermission()) {
+                    openCamera();
+                } else {
+                    requestPermission();
+                }
+            }
+        });
+    }
+
+    /**
+     * checks whether all the needed permissions have been granted or not
+     *
+     * @param grantResults the permission grant results
+     * @return true if all the reqested permission has been granted,
+     * otherwise returns false
+     */
+    private boolean hasAllPermissions(int[] grantResults) {
+        for (int result : grantResults) {
+            if (result == PackageManager.PERMISSION_DENIED)
+                return false;
+        }
+        return true;
+    }
+
+    /**
+     * Method requests for permission if the android version is marshmallow or above
+     */
+    private void requestPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            // whether permission can be requested or on not
+            if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+                Toast.makeText(this, "Camera Permission Required", Toast.LENGTH_SHORT).show();
+            }
+            // request the camera permission permission
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+    /**
+     * checks whether camera permission is available or not
+     *
+     * @return true if android version is less than marshmallo,
+     * otherwise returns whether camera permission has been granted or not
+     */
+    private boolean hasPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            return checkSelfPermission(Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED;
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        // if this is the result of our camera permission request
+        if (requestCode == CAMERA_PERMISSION_REQUEST_CODE) {
+            if (hasAllPermissions(grantResults)) {
+                openCamera();
+            } else {
+                requestPermission();
+            }
+        }
+    }
+
+    /**
+     * creates and starts an intent to get a picture from camera
+     */
+    private void openCamera() {
+        Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQEUST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        // if this is the result of our camera image request
+        if (requestCode == CAMERA_REQEUST_CODE) {
+            // getting bitmap of the image
+            Bitmap photo = (Bitmap) Objects.requireNonNull(Objects.requireNonNull(data).getExtras()).get("data");
+            // displaying this bitmap in imageview
+            imageView.setImageBitmap(photo);
+
+            // pass this bitmap to classifier to make prediction
+            List<ImageClassifier.Recognition> predicitons = imageClassifier.recognizeImage(
+                    photo, 0);
+
+            // creating a list of string to display in list view
+            final List<String> predicitonsList = new ArrayList<>();
+            for (ImageClassifier.Recognition recog : predicitons) {
+                predicitonsList.add(recog.getName() + "  ::::::::::  " + recog.getConfidence());
+            }
+
+            // creating an array adapter to display the classification result in list view
+            ArrayAdapter<String> predictionsAdapter = new ArrayAdapter<>(
+                    this, R.layout.support_simple_spinner_dropdown_item, predicitonsList);
+            listView.setAdapter(predictionsAdapter);
+
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
 
     //EditText
     @Override
